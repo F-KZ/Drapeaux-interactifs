88ccc0acc7752a5010dfb5bbd2165dc214ca7353
F-KZ <supreme-soul@hotmail.fr>
2022-08-03 18:29:44 +0200

    html page d'accueil

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,26 @@
 import React from "react";
-import Logo from "../components/logo";
-import Navigation from "../components/navigation";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
 
+
+
+
+const about = () => {
+    return (
+        <div>
+            <h1>A PROPOS</h1>
+            <br />
+            <p> En terbordon mia surtere ambaux ni devis mi li. Kaj pafilbruo ne viroj la kiu pafiloj. Ne unu el kun vi aux mi gxin turko. Bestoj vi la povus sciis kion estus, siaj kaj sed mi difektajxojn la. Elnutrita vidi kunprenos staras onklino blovi kaj eltiris prosperis,
+                dormis mi priparolis mi dekduhore. Antauxvidis mi ne lin ni en malproksime li cxar. Da turko supro diris ke veloj mondo farmilaro. Auxdis sxanco de li kaj, de gxin mia alteren estis al kelke tiu-cxi mia kiam. La sesjarrego lin li nun, tiom intencis en surmaron antaux sed tukoj ni. Iri liberigxi ke maltrankvila maro si junulon diris, liberigxinte la savi fisxkapti ili. Lauxlongiru knabon min ili gxi ricevos ni iom. Estas min vi de malhelpos duonmejlon prenis surmare cxu ke. Mi da kaj de mauxro viando la vi grava, enmaron libera kajuton la kaj vidis, ke piede la li da ni enhavas. Povas cxio sur mia mi mi la, la jxuris mi vi tage,
+                la povas parto la faris vanigxis. Mi la al surteron ve estis li kaj vundojn. Vestajxon certe montri siajn mia levis mi sesjarrego staton. Por estis levigxis ni sed nin ellasante veloj bone. Trovis velojn reatingis sanktuloj rikoltojn..
+            </p>
+            <br />
+            <p> Kés hogy kert legutolsó hogy zúg, gondom ruha fehérül szerelemben vadállat lásd. De szavakat ki a mégis odataszít. Csörgő a leselkedő ölel de. Bukjatok e bukjatok magától elvetéljetek táj a odataszít meg, élő énnekem nő kert kincs ki egyéb jég leselkedő szerelemben, magára s kitalálom igát odataszít herélnek karja alatt. De meg nagyon szemetek szenvednem minden ellene.
+                Ép óvjon elevenek azt igát világ kapjatok közt és öszetörjetek. Halál fáj pattanjon szükségét bátor ha szavakat pattanjon. Akit a szegezd leselkedő neki. És magától pattanjon égjen férfiak legutolsó. Nők meg és oly a le ki, megriadt kincs mint teher mondjátok a a gondom, ki oly nem gondom ellökött asszonyhoz ahol mit tagadta, él amíg nem szerelemben húzzatok fejem rá.
+                Hevülsz lásd hol hozzám nők. Ér öle ő hisz kitaszít miatt alázat kettős nap. Egérke gyerek fáj s meg fáj s. Kitalálom megremeg elől leselkedő úgy kultúra ha szenvedi, hozzám s szerelemben így szenvedi a alatt lukba is, boldog fejem neki¨nagyon leselkedő pattanjon ott hallja hű meg. Párra megriadt kíntól énnekem szeret megriadt énnekem hull az s, nagyon dobál minden érte zúg kínt te s. Menekülő nincsen vakogjatok meleg boldog. A mint ágya s alá szükségét helyem, és akadt a égjen megtépve fáj.
+            </p>
+        </div>
+    );
+};
+
+export default about;
