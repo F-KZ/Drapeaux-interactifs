02b3d7712bc4042f7b1ac36807d1718d4b6885a8
F-KZ <supreme-soul@hotmail.fr>
2022-08-19 17:47:22 +0200

    probleme index a regler

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <li className="card">
+      <div className="infos">
+        <h2>Nom du pays</h2>
+      </div>
+    </li>
+  );
+};
+
+export default card;
